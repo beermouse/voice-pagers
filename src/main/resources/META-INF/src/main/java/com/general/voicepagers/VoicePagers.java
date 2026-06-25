1a83920f94b4b9bd89fcf5658f90a93ff02f4860
beermouse <7ewwwa@gmail.com>
2026-06-25 22:23:26 +0500

    Update VoicePagers.java

--- src/main/resources/META-INF/src/main/java/com/general/voicepagers/VoicePagers.java
+++ src/main/resources/META-INF/src/main/java/com/general/voicepagers/VoicePagers.java
@@ -3,10 +3,15 @@ package com.general.voicepagers;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,22 +22,17 @@ import net.minecraftforge.registries.RegistryObject;
 public class VoicePagers {
     public static final String MODID = "voicepagers";
 
-    // Создаем регистратор для предметов
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Регистрируем наш пейджер через созданный нами класс PagerItem
     public static final RegistryObject<Item> PAGER = ITEMS.register("pager", 
         () -> new PagerItem(new Item.Properties().stacksTo(1)));
 
     public VoicePagers() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Подключаем регистратор к шине событий мода
         ITEMS.register(modEventBus);
-
-        // Добавляем пейджер в ванильную вкладку инструментов в креативе
         modEventBus.addListener(this::addCreative);
 
+        // Регистрируем наш класс в главной шине событий, чтобы ловить сообщения чата
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -40,5 +40,51 @@ public class VoicePagers {
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(PAGER);
         }
+    }
+
+    // Ловим отправку сообщений в чат
+    @SubscribeEvent
+    public void onServerChat(ServerChatEvent event) {
+        ServerPlayer sender = event.getPlayer();
+        String message = event.getRawText();
+
+        // Если сообщение начинается с "!", считаем это отправкой в пейджер
+        if (message.startsWith("!")) {
+            int frequency = getPlayerPagerFrequency(sender);
+
+            // Если у игрока есть настроенный пейджер
+            if (frequency > 0) {
+                // Отменяем отправку сообщения в глобальный обычный чат
+                event.setCanceled(true);
+
+                // Отрезаем "!" от текста
+                String pagerMessage = message.substring(1).trim();
+                
+                // Форматируем красивый текст: [Пейджер CH-3] Игрок: Текст
+                Component formattedText = Component.literal("§8[§bПейджер СН-" + frequency + "§8] §7" + sender.getScoreboardName() + "§r: " + pagerMessage);
+
+                // Рассылаем всем игрокам, у кого есть пейджер на этой же частоте
+                for (ServerPlayer receiver : sender.getServer().getPlayerList().getPlayers()) {
+                    if (getPlayerPagerFrequency(receiver) == frequency) {
+                        receiver.sendSystemMessage(formattedText);
+                    }
+                }
+            }
+        }
+    }
+
+    // Метод поиска пейджера в инвентаре
+    private int getPlayerPagerFrequency(ServerPlayer player) {
+        Inventory inventory = player.getInventory();
+        for (int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack stack = inventory.getItem(i);
+            if (!stack.isEmpty() && stack.getItem() == PAGER.get()) {
+                CompoundTag nbt = stack.getTag();
+                if (nbt != null && nbt.contains("frequency")) {
+                    return nbt.getInt("frequency");
+                }
+            }
+        }
+        return 0;
     }
 }
