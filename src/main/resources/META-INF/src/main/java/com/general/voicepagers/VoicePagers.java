210cfa4c77ee0e24ccef178eeab4e2929df96920
beermouse <7ewwwa@gmail.com>
2026-06-25 21:50:52 +0500

    Update VoicePagers.java

--- src/main/resources/META-INF/src/main/java/com/general/voicepagers/VoicePagers.java
+++ src/main/resources/META-INF/src/main/java/com/general/voicepagers/VoicePagers.java
@@ -1,44 +1,59 @@
 package com.general.voicepagers;
 
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.network.chat.Component;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-@Mod(VoicePagers.MODID)
-public class VoicePagers {
-    public static final String MODID = "voicepagers";
+import java.util.List;
 
-    // Создаем регистратор для предметов
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    // Регистрируем наш пейджер со стандартными свойствами (стакается до 1 штуки)
-    public static final RegistryObject<Item> PAGER = ITEMS.register("pager", 
-        () -> new Item(new Item.Properties().stacksTo(1)));
-
-    public VoicePagers() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Подключаем регистратор к шине событий мода
-        ITEMS.register(modEventBus);
-
-        // Добавляем пейджер в ванильную вкладку инструментов в креативе
-        modEventBus.addListener(this::addCreative);
-
-        MinecraftForge.EVENT_BUS.register(this);
+public class PagerItem extends Item {
+    public PagerItem(Properties properties) {
+        super(properties);
     }
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-            event.accept(PAGER);
+    // Метод срабатывает при клике правой кнопкой мыши (ПКМ) с пейджером в руке
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+
+        // Будем менять частоту только на стороне сервера, чтобы избежать рассинхрона
+        if (!level.isClientSide()) {
+            CompoundTag nbt = stack.getOrCreateTag();
+            
+            // Считываем текущую частоту (если её нет, вернётся 0) и циклически гоняем от 1 до 9
+            int currentFrequency = nbt.getInt("frequency");
+            int newFrequency = currentFrequency >= 9 ? 1 : currentFrequency + 1;
+            
+            nbt.putInt("frequency", newFrequency);
+            
+            // Выводим сообщение игроку в чат
+            player.sendSystemMessage(Component.literal("§a[Пейджер] §fНастроена частота: §e" + newFrequency + " МГц"));
         }
+
+        return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+    }
+
+    // Добавляем красивое описание предмета в инвентаре при наведении
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+        int frequency = 0;
+        if (stack.hasTag() && stack.getTag().contains("frequency")) {
+            frequency = stack.getTag().getInt("frequency");
+        }
+        
+        if (frequency > 0) {
+            tooltip.add(Component.literal("§7Частота: §e" + frequency + " МГц"));
+        } else {
+            tooltip.add(Component.literal("§8§oПейджер не настроен. Нажмите ПКМ."));
+        }
+        
+        super.appendHoverText(stack, level, tooltip, flag);
     }
 }
