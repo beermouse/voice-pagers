56bf208c53fd7fa707a45178abe95f00ffd658b9
beermouse <7ewwwa@gmail.com>
2026-06-25 23:52:08 +0500

    Create PagerItem.java

---
+++ src/main/java/com/general/voicepagers/PagerItem.java
@@ -0,0 +1,9 @@
+package com.general.voicepagers;
+
+import net.minecraft.world.item.Item;
+
+public class PagerItem extends Item {
+    public PagerItem(Properties properties) {
+        super(properties);
+    }
+}
