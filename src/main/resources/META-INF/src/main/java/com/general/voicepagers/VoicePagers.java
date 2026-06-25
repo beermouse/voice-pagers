d2f2df31d638c588ad68c2e74258be5b30cd361c
beermouse <7ewwwa@gmail.com>
2026-06-25 21:46:43 +0500

    Update VoicePagers.java

--- src/main/resources/META-INF/src/main/java/com/general/voicepagers/VoicePagers.java
+++ src/main/resources/META-INF/src/main/java/com/general/voicepagers/VoicePagers.java
@@ -2,53 +2,43 @@ package com.general.voicepagers;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(VoicePagers.MODID)
 public class VoicePagers {
     public static final String MODID = "voicepagers";
-    public static final Logger LOGGER = LogManager.getLogger();
 
-    // Создаем регистратор для наших предметов
+    // Создаем регистратор для предметов
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Временно регистрируем пейджер как обычный базовый предмет (в следующих шагах мы дадим ему логику и NBT)
+    // Регистрируем наш пейджер со стандартными свойствами (стакается до 1 штуки)
     public static final RegistryObject<Item> PAGER = ITEMS.register("pager", 
         () -> new Item(new Item.Properties().stacksTo(1)));
 
     public VoicePagers() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Регистрируем наш список предметов в системе Forge
+        // Подключаем регистратор к шине событий мода
         ITEMS.register(modEventBus);
 
-        // Подключаем метод добавления в креативный инвентарь
+        // Добавляем пейджер в ванильную вкладку инструментов в креативе
         modEventBus.addListener(this::addCreative);
 
-        // Регистрируем сам мод в главном автобусе событий Майнкрафта
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    // Добавляем наш пейджер во вкладку "Инструменты" в креативе
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(PAGER);
         }
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("Мод Voice Pagers успешно запущен на сервере!");
     }
 }
